a6450ff1cc36e4a1495c76741f28e0ff1e5bcdc5
gabriel-oll <gabb.webdev@gmail.com>
2022-08-03 19:43:15 -0300

    adding create functionality

--- api/src/models/Profile.js
+++ api/src/models/Profile.js
@@ -1,5 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const Profile = mongoose.model('Profile', {
-
+    username: String,
+    avatar: String,
+    profileUrl: String,
+    bio: String,
+    repos: String,
+    qtdRepos: Number,
+    followers: Number,
 })
+
+module.exports = Profile
--- api/src/server/index.js
+++ api/src/server/index.js
@@ -3,36 +3,54 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+const Profile = require('../models/Profile')
+const fetch = (url) => import('node-fetch').then(({default: fetch}) => fetch(url));
 
-//  middlewares
-app.use(
-  express.urlencoded( {
-      extended: true
-  } ),
-  express.json()
-);
-
-// routes
-app.get('/', (req, res) => {
-  res.send( {
-    message: "api test"
-  } )
-});
-
-// Blhv9PjkyzRcsDmh
 
 const db_user = process.env.DB_USER
 const db_pass = process.env.DB_PASSWORD
 
-// mongodb+srv://gabrieloll:<password>@gh-api-cluster.nkavpon.mongodb.net/?retryWrites=true&w=majority
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-mongoose
-.connect(`mongodb+srv://${db_user}:${db_pass}@gh-api-cluster.nkavpon.mongodb.net/?retryWrites=true&w=majority`)
-.then(
-  () => {
-    console.log('conection success')
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
+// routes
+
+// create
+app.post('/profile', (req, res) => {
+
+let userProfile  = req.body.user
+
+let url = `https://api.github.com/users/${userProfile}`
+fetch(url)
+.then(function(response){ 
+  return response.json()
+})
+.then(function(data) {
+  const userLogs = {
+    username: data.login,
+    avatar: data.avatar_url, 
+    profileUrl: data.html_url,
+    bio: data.bio,
+    qtdRepos: data.public_repos,
+    followers: data.followers,
   }
-);
+  Profile.create(userLogs)
+  res.send(userLogs)  
+})
+})
+
+// app.get('/', (req, res) => {
+//   res.send()
+// })
+mongoose
+  .connect(`mongodb+srv://${db_user}:${db_pass}@gh-api-cluster.nkavpon.mongodb.net/?retryWrites=true&w=majority`)
+  .then(
+    () => {
+      console.log('conection success')
+      app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+      })
+    }
+  );
