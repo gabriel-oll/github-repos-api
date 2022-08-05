a27fac80608c6ca6db8861d8d80b16b5450a9559
gabriel-oll <gabb.webdev@gmail.com>
2022-08-05 10:58:39 -0300

    adding port 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 80
 const profilesRouter = require('./api/src/routes/ProfilesRouter')
 
 const db_user = process.env.DB_USER
